3c0154e20fb80e729ab8712436c33cb58dfd22b5
Antonio Rendon <mentalshine76@gmail.com>
2018-11-27 23:09:43 -0300

    first commit: 3 rough models (users with devise, events with model g and products with model)

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :products, only: :index
+  resources :events, only: :index
+  root to: 'events#index'
+
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
