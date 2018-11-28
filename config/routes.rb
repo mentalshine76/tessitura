b1a2d8d0c2ce26bcf310175b2d1d39ed05c94b61
Antonio Rendon <mentalshine76@gmail.com>
2018-11-28 12:30:22 -0300

    Third commit: addition of name field in the registration form, and inclusion of name as strong param in the registrations controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :events, only: :index
   root to: 'events#index'
 
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
